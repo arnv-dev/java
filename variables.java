df4a9981d7fe20339dddb3a004d0ec85a32b3ae6
Arnav <arnavkapadnis207@gmail.com>
2026-07-09 20:11:24 +0530

    variables

---
+++ variables.java
@@ -0,0 +1,37 @@
+
+public class variables{
+
+     public static void main(String[] args) {
+    // Declaring and initializing variables
+        
+        // Integer variable  
+        int age = 25;       
+        
+        // String variable  
+        String name = "GeeksforGeeks"; 
+        
+        // Double variable 
+        double salary = 50000.50;     
+
+         // Initializing float variable
+        float si = 5.5f; 
+        
+        // Initializing integer variables
+        int t = 10; 
+        int s = 20; 
+        
+        // Initializing character variable
+        char var = 'h'; 
+        
+      
+
+        // Displaying the values of variables
+        System.out.println("Simple Interest: " + si);  
+        System.out.println("Speed: " + s);                      
+        System.out.println("Time: " + t);                        
+        System.out.println("Character: " + var); 
+        System.out.println("Age: " + age);          
+        System.out.println("Name: " + name);        
+        System.out.println("Salary: " + salary);
+     }
+}
