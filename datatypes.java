657c837a1fab728a078b796bdc64e2d68edd092c
Arnav <arnavkapadnis207@gmail.com>
2026-07-31 18:28:35 +0530

    datatypes in java

--- datatypes.java
+++ datatypes.java
@@ -1,3 +1,14 @@
 public class datatypes {
-    
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Is fish tasty? " + isFishTasty);
+
+        byte age = 25;
+        byte temperature = -10;
+        System.out.println("Age: " + age);
+        System.out.println("Temperature: " + temperature);
+    }
 }
+
